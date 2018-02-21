48ee95e3133fa508c65c961db11ed37255fe62ad
divasatanica <jayhebes@vip.qq.com>
2018-02-21 22:51:11 +0800

    update: 豆瓣电影榜爬取并发送邮件的任务

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const getMovieRank = require('./stuff/movies/douban-rank')
 const bootOptions = require('./config/boot')
 
 const movieHTMLRender = require('./stuff/movies/h_gen')
+const request = require('superagent')
 
-bootOptions.headless = true
+bootOptions.headless = false
 
 let _args = process.argv
 
@@ -12,7 +13,16 @@ switch (_args[3]) {
     case 'douban-top10': {
         getMovieRank(envOpt() || bootOptions).then(data => {
             let html = movieHTMLRender(data)
-            sendMail('豆瓣电影排行榜', html)
+            let date = new Date()
+            request.put('http://localhost:9608/mail/s001')
+                   .send({
+                       subject: `${date.toLocaleDateString()} 豆瓣电影榜`,
+                       content: html,
+                       service: 'qq'
+                   })
+                   .end(res => {
+                       console.log(res)
+                   })
         }).catch(e => {
             console.log(e)
         })
